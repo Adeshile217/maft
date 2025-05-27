7370d2c911c924b4d84082580f627de9429a973a
Adeshile Ibraheem <sadeshile@gmail.com>
2025-05-27 07:38:59 -0400

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,14 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
+  title: 'MAFT',
   description: 'Created with v0',
   generator: 'v0.dev',
+  icons:{
+    icon:["./favicon.ico?v=4"],
+    apple:["./apple-touch-icon.png?v=4"],
+    shortcut:["./apple-touch-icon.png"]
+  }
 }
 
 export default function RootLayout({
